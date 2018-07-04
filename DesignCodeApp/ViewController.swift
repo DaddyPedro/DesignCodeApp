72194ce28c2aa4620aa7ce17fd1491dfa327541b
mhd tahhan <mhd_tahhan@hotmail.com>
2018-07-04 20:52:46 +0300

    AVPlayer kit added with player controller, alpha animate function added, section view controller added.

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualEffectView: UIVisualEffectView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        alphaAnimation(dutation: 3)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
+    
+    //Mark: - Alpha animation
+    func alphaAnimation(dutation: Double) {
+        
+        titleView.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualEffectView.alpha = 0
+        
+        UIView.animate(withDuration: dutation) {
+            self.titleView.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualEffectView.alpha = 1
+        }
+    }
+    
+    
+    
+    // Mark: - Play button tapped
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        let url = URL(string: urlString)
+        
+        let player = AVPlayer(url: url!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+        
+    }
+    
+    
 
 }
 
