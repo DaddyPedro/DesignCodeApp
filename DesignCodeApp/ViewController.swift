676a7bc8d57d0ec2b7710f88d8755be31016cebd
mhd tahhan <mhd_tahhan@hotmail.com>
2018-07-07 13:45:02 +0300

    Applied animation for the cell layer using catransform3dconcat on a separate function.

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -55,6 +55,7 @@ class ViewController: UIViewController {
     
     
     // Mark: - Play button tapped
+    
     @IBAction func playButtonTapped(_ sender: Any) {
         let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
         let url = URL(string: urlString)
@@ -72,6 +73,8 @@ class ViewController: UIViewController {
 
 }
 
+// Mark: - Cell conformace protocol
+
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return sections.count
@@ -82,6 +85,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         cell.cellImageView.image = UIImage(named: section["image"]!)
         cell.cellTitleLabel.text = section["title"]
         cell.cellCaptionLabel.text = section["caption"]
+        cell.layer.transform = animateCell(cellFrame: cell.frame)
         
         return cell
     }
@@ -94,6 +98,9 @@ extension ViewController: UIScrollViewDelegate {
     
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        // Mark: - Parallax animation for hero view
+        
         let offsety = scrollView.contentOffset.y
         if offsety < 0 {
             heroView.transform = CGAffineTransform(translationX: 0, y: offsety)
@@ -102,6 +109,57 @@ extension ViewController: UIScrollViewDelegate {
             titleView.transform = CGAffineTransform(translationX: 0, y: -offsety/4)
             homeImageView.transform = CGAffineTransform(translationX: 0, y: -offsety/5)
         }
+        
+        if let collectionView = scrollView as? UICollectionView {
+            
+            // Mark: - Cell background translation using scroll view
+
+            for cell in collectionView.visibleCells as! [SectionCollectionViewCell] {
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = collectionView.convert(attributes.frame, to: view)
+               let tranlationX = cellFrame.origin.x/5
+                cell.cellImageView.transform = CGAffineTransform(translationX: tranlationX, y: 0)
+                
+                cell.layer.transform = animateCell(cellFrame: cellFrame)
+        }
     }
-}
+    
+
+        }
+    
+    // Mark: - Cell animation function
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        
+
+        let angleFromX = Double(cellFrame.origin.x / 10)
+        let angle = CGFloat((angleFromX * Double.pi) / 180)
+        
+        var transform = CATransform3DIdentity
+        transform.m34 = 1.0/1000
+        
+        
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200))/1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        if scaleFromX > scaleMax {
+            scaleFromX = scaleMax
+            
+        }else if scaleFromX < scaleMin {
+            scaleFromX = scaleMin
+        }
+        
+        let scale = CATransform3DScale(transform, scaleFromX, scaleFromX, 1)
+        
+        return CATransform3DConcat(rotation, scale)
+    }
+    
+    }
+    
+    
+    
+
 
